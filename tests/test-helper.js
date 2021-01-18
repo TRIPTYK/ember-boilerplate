40a0cc298d3ed38898461b1e26b7c44be73d747d
amauryD <deflorenne.amaury@gmail.com>
2021-01-18 12:01:13 +0100

    v3.22.0...v3.24.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
 import Application from 'ember-boilerplate/app';
 import config from 'ember-boilerplate/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
