80529c39b51222ef55c8dc29e5c407e84c1da101
AmauryD <deflorenne.amaury@gmail.com>
2021-07-13 16:48:37 +0200

    fixes typescript compilation and downgrade ember data

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -32,6 +32,7 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  const { Webpack } = require('@embroider/webpack');
 
-  return app.toTree();
+  return require('@embroider/compat').compatBuild(app, Webpack);
 };
