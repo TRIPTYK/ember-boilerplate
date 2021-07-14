ca43518328212deb2b5f50ca44892c1ddfcda6e3
Gilles BERTRAND <gilles@triptyk.eu>
2021-07-14 10:47:22 +0200

    fix tailwind jit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -31,8 +31,6 @@ module.exports = function (defaults) {
   // If the library that you are including contains AMD or ES6
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-  const { Webpack } = require('@embroider/webpack');
-
-  return require('@embroider/compat').compatBuild(app, Webpack);
+  // alon g with the exports of each module as its value.
+  return app.toTree();
 };
