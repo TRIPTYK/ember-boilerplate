0358643dc15457fbc0cdc886852ecfdc7be7942b
AmauryD <deflorenne.amaury@gmail.com>
2022-01-26 21:18:45 +0100

    working css themes

--- app/tailwind/tailwind.config.js
+++ app/tailwind/tailwind.config.js
@@ -1,18 +1,28 @@
+/* eslint-disable no-undef */
+const colors = require('tailwindcss/colors');
+
 /* eslint-disable */
 module.exports = {
-  content:  ['./app/**/*.{hbs,js,ts,html}', './tests/**/*.{hbs,js,ts,html}'],
+  content: ['./app/**/*.{hbs,js,ts,html}', './tests/**/*.{hbs,js,ts,html}'],
   corePlugins: {},
   plugins: [],
-  screens: {
-    lxg: '1140px',
-    '2xl': '1600px',
-    '3xl': '1680px',
+  theme: {
+    extends: {
+      screens: {
+        lxg: '1140px',
+        '2xl': '1600px',
+        '3xl': '1680px',
+      },
+    },
+    colors: {
+      primary: '#CFAF6E',
+      secondary: '#2B4999',
+      text: '#647377',
+      'text-secondary': '#384043',
+      error: '#D72F33',
+      warn: '#ffcc00',
+      white : colors.white,
+      black: colors.black
+    },
   },
-  colors: {
-    primary: '#CFAF6E',
-    secondary: '#2B4999',
-    text: '#647377',
-    'text-secondary': '#384043',
-    error: '#D72F33'
-  }
 };
