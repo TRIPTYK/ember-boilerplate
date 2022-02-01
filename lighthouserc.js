b94c7b6cbdf2016d1572cbfbffcb506c307c0986
AmauryD <deflorenne.amaury@gmail.com>
2022-02-01 15:14:11 +0100

    ligthouse

---
+++ lighthouserc.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000'],
+      startServerCommand: 'ember s -p=3000',
+    },
+  },
+};
