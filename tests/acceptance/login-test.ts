ca505dceb1489266137d9ac47a7d2e609b2e1d95
AmauryD <deflorenne.amaury@gmail.com>
2022-02-02 15:19:50 +0100

    update deps

--- tests/acceptance/login-test.ts
+++ tests/acceptance/login-test.ts
@@ -3,17 +3,16 @@ import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import fillIn from '@ember/test-helpers/dom/fill-in';
 import click from '@ember/test-helpers/dom/click';
+import type { MirageTestContext } from 'ember-cli-mirage/test-support';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Acceptance | login', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('visiting /login', async function (assert) {
-    // @ts-expect-error
+  test<MirageTestContext>('visiting /login', async function (assert) {
     this.server.createList('user', 1);
     await visit('/login');
-    await this.pauseTest();
     await fillIn(
       "[data-test-form-login] [data-test-input='email']",
       'dev@triptyk.eu'
