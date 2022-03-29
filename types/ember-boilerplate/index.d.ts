dab174c8a5582334b1d88e02ebb68bba5cc9337b
AmauryD <deflorenne.amaury@gmail.com>
2022-03-29 23:27:39 +0200

    up deps

--- types/ember-boilerplate/index.d.ts
+++ types/ember-boilerplate/index.d.ts
@@ -1,8 +1,11 @@
-import Ember from 'ember';
+import type Ember from 'ember';
+import type Route from '@ember/routing/route';
 
 declare global {
   interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {}
   // interface Function extends Ember.FunctionPrototypeExtensions {}
+  type Resolved<P> = P extends Promise<infer T> ? T : P;
+  type ModelFrom<R extends Route> = Resolved<ReturnType<R['model']>>;
 }
 
 export {};
