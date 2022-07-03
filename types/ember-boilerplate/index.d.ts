43821b20e80771433914f07cb66149f8f9044e18
AmauryD <deflorenne.amaury@gmail.com>
2022-07-03 18:00:37 +0200

    fix type info

--- types/ember-boilerplate/index.d.ts
+++ types/ember-boilerplate/index.d.ts
@@ -1,9 +1,8 @@
-import type Ember from 'ember';
 import type Route from '@ember/routing/route';
+import type ArrayPrototypeExtensions from '@ember/array/types/prototype-extensions';
 
 declare global {
-  interface Array<T> extends Ember.ArrayPrototypeExtensions<T> {}
-  // interface Function extends Ember.FunctionPrototypeExtensions {}
+  interface Array<T> extends ArrayPrototypeExtensions<T> {}
   type Resolved<P> = P extends Promise<infer T> ? T : P;
   type ModelFrom<R extends Route> = Resolved<ReturnType<R['model']>>;
 }
