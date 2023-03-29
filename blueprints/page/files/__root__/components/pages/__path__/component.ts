5e5119af2d7ba95fcda36cba00bc475a22975a58
AmauryD <deflorenne.amaury@gmail.com>
2023-03-29 12:23:26 +0200

    remove blueprints

--- blueprints/page/files/__root__/components/pages/__path__/component.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable unicorn/filename-case */
-import Component from '@glimmer/component';
-
-interface Pages<%= classifiedModuleName %>IndexArgs {}
-
-export default class Pages<%= classifiedModuleName %>Index extends Component<Pages<%= classifiedModuleName %>IndexArgs> {
-}
