85f2d0272df4c7c11ae35ccca57f4fa455e1d9a5
AmauryD <deflorenne.amaury@gmail.com>
2023-04-01 20:45:40 +0200

    mocking in development

--- app/application/route.ts
+++ app/application/route.ts
@@ -3,6 +3,8 @@ import { service } from '@ember/service';
 import type CurrentUserService from 'ember-boilerplate/services/current-user';
 import type IntlService from 'ember-intl/services/intl';
 import type SessionService from 'ember-simple-auth/services/session';
+import { registerDestructor } from '@ember/destroyable';
+import config from 'ember-boilerplate/config/environment';
 
 export default class Application extends Route {
   @service declare session: SessionService;
@@ -10,8 +12,22 @@ export default class Application extends Route {
   @service declare currentUser: CurrentUserService;
 
   async beforeModel() {
+    await this.setupMSWForDevelopment();
     this.intl.setLocale(['fr-fr']);
     await this.session.setup();
     await this.currentUser.load();
   }
+
+  private async setupMSWForDevelopment() {
+    if (config.environment === 'development') {
+      await setupMSW(this);
+    }
+  }
+}
+
+async function setupMSW(context: object) {
+  // @ts-expect-error
+  let { worker } = await import('/mocks/index.js');
+  await worker.start();
+  registerDestructor(context, () => worker.stop());
 }
---
+++ public/mocks/handlers.js
@@ -0,0 +1,5 @@
+import { usersHandlers } from "./handlers/users";
+
+export const handlers = [
+  ...usersHandlers
+];
