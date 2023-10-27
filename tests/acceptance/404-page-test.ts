9708c054f1409f53c2970a1ffe3050fa1467b260
remadex <s.cardon@outlook.com>
2023-10-27 14:33:01 +0200

    add setupIntl

--- tests/acceptance/404-page-test.ts
+++ tests/acceptance/404-page-test.ts
@@ -2,10 +2,13 @@ import { module, test } from 'qunit';
 
 import { setupApplicationTest } from 'ember-boilerplate/tests/helpers';
 
+import { setupIntl } from 'ember-intl/test-support';
+
 import { notFoundPage } from '../pages/404';
 
 module('Acceptance | 404 page', function (hooks) {
   setupApplicationTest(hooks);
+  setupIntl(hooks, ['fr-fr']);
 
   test('visiting /404-page', async function (assert) {
     await notFoundPage.visit();
