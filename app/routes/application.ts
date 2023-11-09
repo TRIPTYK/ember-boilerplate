96b8a2976f6cefee90f9a4b0354c2c66893a7e21
AmauryD <deflorenne.amaury@gmail.com>
2023-11-09 09:49:47 +0100

    set locale

--- app/routes/application.ts
+++ app/routes/application.ts
@@ -6,14 +6,17 @@ import config from 'ember-boilerplate/config/environment';
 import { usersHandlers } from 'ember-boilerplate/handlers/users';
 
 import type CurrentUserService from 'ember-boilerplate/services/current-user';
+import type { IntlService } from 'ember-intl';
 import type SessionService from 'ember-simple-auth/services/session';
 
 export default class Application extends Route {
   @service declare session: SessionService;
   @service declare currentUser: CurrentUserService;
+  @service declare intl: IntlService;
 
   async beforeModel() {
     await this.setupMSWForDevelopment();
+    this.intl.setLocale(['fr-fr']);
     await this.session.setup();
     await this.currentUser.load();
   }
